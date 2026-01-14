f0e86d3cedf553f3ed272399293009185e773799
Xenon506 <yuvrajsingh232415@gmail.com>
2026-01-14 11:03:19 +0530

    Exam day

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,6 @@
 import 'package:bussiness_by_jinn/bussinesspage5.dart';
 import 'package:bussiness_by_jinn/companiespage.dart';
-import 'package:bussiness_by_jinn/companiespage4.dart';
+
 import 'package:bussiness_by_jinn/companiespage6.dart';
 import 'package:flutter/material.dart';
 
@@ -10,43 +10,44 @@ class Home extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Placeholder(
-      color: Colors.black,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(onPressed: () {
-            Navigator.push( 
-              context,
-               MaterialPageRoute(builder: (context) => Companiespage()),
-            );
-          }, child: const Text('companies page 1')),
-
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.cyan[800],
         
-        // ElevatedButton(onPressed: (){
-        //     Navigator.push(context, 
-        //     MaterialPageRoute(builder: (context) => Companiespage3(bid: '1')));
-        //   }, child: Text('companies page 3')),
-
-          ElevatedButton(onPressed: (){
-            Navigator.push(context, 
-            MaterialPageRoute(builder: (context) => Companiespage4()));
-          }, child: Text('companies page 4')),
-
-          ElevatedButton(onPressed: (){
-            Navigator.push(context, 
-            MaterialPageRoute(builder: (context) => companiespage5()));
-          }, child: Text('companies page 5')),
-          
-          ElevatedButton(onPressed: (){
-            Navigator.push(context, 
-            MaterialPageRoute(builder: (context) => Companiespage6()));
-          }, child: Text('companies page 6')),
-
-
-
-        ],
       ),
+      body: 
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {
+              Navigator.push( 
+                context,
+                 MaterialPageRoute(builder: (context) => Companiespage()),
+              );
+            }, child: const Text('companies page 1')),
+        
+          
+          // ElevatedButton(onPressed: (){
+          //     Navigator.push(context, 
+          //     MaterialPageRoute(builder: (context) => Companiespage3(bid: '1')));
+          //   }, child: Text('companies page 3')),
+        
+        
+            ElevatedButton(onPressed: (){
+              Navigator.push(context, 
+              MaterialPageRoute(builder: (context) => companiespage5()));
+            }, child: Text('companies page 5')),
+            
+            ElevatedButton(onPressed: (){
+              Navigator.push(context, 
+              MaterialPageRoute(builder: (context) => Companiespage6()));
+            }, child: Text('companies page 6')),
+        
+        
+        
+          ],
+        ),
+      
     );
   }
 }
